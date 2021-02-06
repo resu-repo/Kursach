bd650be6b1d6b860bc776326e6d0764b7c420a4c
Resu <Resu@DESKTOP-UAMN0M3>
2021-02-06 17:13:37 +0300

    1

--- Kursach/Var_16.Designer.cs
+++ Kursach/Var_16.Designer.cs
@@ -30,8 +30,6 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Var_16));
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
@@ -39,6 +37,7 @@
             this.listBox3 = new System.Windows.Forms.ListBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -52,28 +51,6 @@
             this.textBox1.Text = resources.GetString("textBox1.Text");
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(240, 136);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(182, 20);
-            this.textBox2.TabIndex = 1;
-            this.textBox2.Text = "Заполнение массива:";
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Случайные целочисленные",
-            "Случайные действительные"});
-            this.comboBox1.Location = new System.Drawing.Point(240, 162);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(182, 21);
-            this.comboBox1.TabIndex = 2;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-            // 
             // textBox3
             // 
             this.textBox3.Location = new System.Drawing.Point(23, 208);
@@ -86,11 +63,14 @@
             // 
             // listBox1
             // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
             this.listBox1.Location = new System.Drawing.Point(197, 208);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(406, 30);
+            this.listBox1.Size = new System.Drawing.Size(406, 29);
             this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // textBox4
             // 
@@ -104,19 +84,25 @@
             // 
             // listBox2
             // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 25;
             this.listBox2.Location = new System.Drawing.Point(197, 269);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(406, 30);
+            this.listBox2.Size = new System.Drawing.Size(406, 29);
             this.listBox2.TabIndex = 6;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
             // 
             // listBox3
             // 
+            this.listBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 25;
             this.listBox3.Location = new System.Drawing.Point(197, 324);
             this.listBox3.Name = "listBox3";
-            this.listBox3.Size = new System.Drawing.Size(406, 30);
+            this.listBox3.Size = new System.Drawing.Size(406, 29);
             this.listBox3.TabIndex = 7;
+            this.listBox3.SelectedIndexChanged += new System.EventHandler(this.listBox3_SelectedIndexChanged);
             // 
             // textBox5
             // 
@@ -126,6 +112,7 @@
             this.textBox5.Size = new System.Drawing.Size(142, 29);
             this.textBox5.TabIndex = 8;
             this.textBox5.Text = "Вывод нового массива";
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
             // 
             // button1
             // 
@@ -136,12 +123,25 @@
             this.button1.TabIndex = 9;
             this.button1.Text = "Конец \r\nзадания";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(291, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(196, 78);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Сгенерировать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Var_16
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.listBox3);
@@ -149,8 +149,6 @@
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Name = "Var_16";
             this.Text = "Курсач вариант 16";
@@ -163,8 +161,6 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.TextBox textBox4;
@@ -172,6 +168,7 @@
         private System.Windows.Forms.ListBox listBox3;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Kursach/Var_16.cs
+++ Kursach/Var_16.cs
@@ -16,35 +16,102 @@ namespace Kursach
         {
             InitializeComponent();
         }
+        public double vvod(TextBox t)
+        {
+            return Convert.ToDouble(t.Text);
+        }
 
+
+        int[] mas = new int[15];
+        string Print_L(int [] mas)
+        {
+            string y = " ";
+            for(int i = 0; i < 15; i++)
+            {
+                y = y + Convert.ToString(mas[i]) + " ";
+            }
+            return y;
+        }
+        void vvod(int[] mas)
+        {
+            Random Random_Number1 = new Random();
+                for(int i = 0; i < 15 ; ++i)
+                    {
+                        mas[i] = Random_Number1.Next(-10, 10);
+                    }
+        }
+        void Find_Min_Max_mas(int[] mas, int minIndex = 0, int maxIndex = 0)
+        {
+            string w = " ";
+            for (int i = 0; i < 15; i++)
+            {
+                if (mas[i] > mas[maxIndex]) maxIndex = i;
+                if (mas[i] < mas[minIndex]) minIndex = i;
+            }
+            int mult = 1, i1 = minIndex < maxIndex ? minIndex : maxIndex;
+            for (int i = 0; i < Math.Abs(maxIndex - minIndex) - 1; i++)
+            {
+                mult *= mas[i1 + 1 + i];
+                w = w + Convert.ToString((mas[i1 + 1 + i])+ " ");
+            }
+            listBox2.Items.Add(mult);
+            listBox3.Items.Add(w);
+
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
 
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
         private void textBox4_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void textBox3_TextChanged(object sender, EventArgs e)
+        private void textBox5_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void listBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear(); 
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            mas = new int[15];
+            vvod(mas);
+            string y = Print_L(mas);
+            listBox1.Items.Add(y);
+            Find_Min_Max_mas(mas);
         }
     }
 }
